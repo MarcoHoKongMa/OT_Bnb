28331f9d4cb0a083c6f13051f83f7b35833c179f
Ting Jian Wu <tingjian.wu@ontariotechu.net>
2022-04-06 15:52:25 -0400

    set up for junit5 on vscode

---
+++ Test/UserTest.java
@@ -0,0 +1,13 @@
+package Test;
+
+import Phase_4.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+
+
+public class UserTest {
+    
+}
